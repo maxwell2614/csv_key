8585c4789dcf427909c41bd6a6ec2d20e34473cc
maxwell2614 <maxkolesnikov14@bk.ru>
2024-06-09 21:38:26 +0300

    merge

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ df['key'] = df.PATRONYMIC_NAME_CYR.str[:3]+df.FIRST_NAME_CYR.str[:3]+df.BIRTH_DA
 
 
 df1=pd.read_excel('Неразрешения.xlsx')
-df1.insert(loc=idx,column='key1', value=new_col)
+df1.insert(loc=idx,column='key', value=new_col)
 
 df1['BIRTH_DAY']=df1['BIRTH_DAY'].astype(str)
 df1['BIRTH_MONTH']=df1['BIRTH_MONTH'].astype(str)
 df1['BIRTH_YEAR']=df1['BIRTH_YEAR'].astype(str)
 
-df1['key1'] = df1.PATRONYMIC_NAME_CYR.str[:3]+df1.FIRST_NAME_CYR.str[:3]+df1.BIRTH_DAY+df1.BIRTH_MONTH+df1.BIRTH_YEAR
+df1['key'] = df1.PATRONYMIC_NAME_CYR.str[:3]+df1.FIRST_NAME_CYR.str[:3]+df1.BIRTH_DAY+df1.BIRTH_MONTH+df1.BIRTH_YEAR
 
-pd.merge(df, df1, on='')
+# pd.merge(df, df1, on='key', how='left')
 
 print(df1)
